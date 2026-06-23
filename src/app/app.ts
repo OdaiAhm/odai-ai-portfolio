4f0b89664ae17605e4760e0e74cd39b939f87fe3
Odai Ahmad <odai.amgd96@gmail.com>
2026-06-24 01:52:11 +0300

    Initial portfolio website

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Education } from './components/education/education';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Certificates } from './components/certificates/certificates';
+import { Contact } from './components/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+
+  imports: [
+    Navbar,
+    Hero,
+    About,
+    Education,
+    Skills,
+    Projects,
+    Certificates,
+    Contact
+  ],
+
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('odai-ai-portfolio');
-}
+
+export class App {}
